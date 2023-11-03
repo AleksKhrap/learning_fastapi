e549ada27b4362dbfb5643d970473887c75f7eed
AleksKhrap <112804992+AleksKhrap@users.noreply.github.com>
2023-11-03 12:25:31 +0300

    Add two types of authentication

---
+++ authentication/basic.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Depends, status, HTTPException
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from models.models import User
+
+app = FastAPI()
+security = HTTPBasic()
+
+USER_DATA = [User(**{"username": "user1", "password": "pass1"}), User(**{"username": "user2", "password": "pass2"})]
+
+
+def authenticate_user(credentials: HTTPBasicCredentials = Depends(security)):
+    user = get_user_from_db(credentials.username)
+    if user is None or user.password != credentials.password:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    return user
+
+
+def get_user_from_db(username: str):
+    for user in USER_DATA:
+        if user.username == username:
+            return user
+    return None
+
+
+@app.get("/protected_resource/")
+async def get_protected_resource(user: User = Depends(authenticate_user)):
+    return {"message": "You have access to the protected resource!", "user_info": user}
