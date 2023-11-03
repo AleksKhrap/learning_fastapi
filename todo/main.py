5a602eb309438fd100ab839e9f96218bf0dc50a4
AleksKhrap <112804992+AleksKhrap@users.noreply.github.com>
2023-11-03 17:36:12 +0300

    Write all functions for all methods

--- todo/main.py
+++ todo/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from models import models, schemas
-from crud import crud
+import crud
 from models.database import SessionLocal, engine
+from typing import List
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -18,12 +19,14 @@ def get_db():
 
 
 # Добавление todo
-@app.post("/todos", response_model=schemas.TodoCreate)
-async def add_todo(todo: schemas.TodoCreate, db: Session = Depends(get_db)):
-    db_todo = crud.get_todo_by_id(db, todo_id=todo.todo_id)
-    if db_todo:
-        raise HTTPException(status_code=400, detail="Запись с таким id уже есть")
-    return crud.add_todo(db=db, todo=todo)
+@app.post("/todos/add_todo", response_model=schemas.TodoCreate)
+async def add_todo(todo_create: schemas.TodoCreate, db: Session = Depends(get_db)):
+    return crud.add_todo(db=db, todo=todo_create)
+
+
+@app.get("/todos", response_model=List[schemas.Todo])
+async def read_todos(db: Session = Depends(get_db)):
+    return crud.get_todos(db=db, skip=0, limit=10)
 
 
 # Получение todo по id
@@ -34,8 +37,8 @@ async def read_todo(todo_id: int, db: Session = Depends(get_db)):
 
 # Обновление todo по id
 @app.put("/todos/update_{todo_id}", response_model=schemas.Todo)
-async def add_todo(todo_id: int, todo: schemas.TodoUpdate, db: Session = Depends(get_db)):
-    db_todo = crud.update_todo(db, todo_id=todo_id, todo=todo)
+async def update_todo(todo_id: int, todo: schemas.TodoUpdate, db: Session = Depends(get_db)):
+    db_todo = crud.update_todo_by_id(db, todo_id=todo_id, todo=todo)
     if db_todo is None:
         raise HTTPException(status_code=404, detail="Запись с таким id не найдена")
     return db_todo
@@ -43,5 +46,8 @@ async def add_todo(todo_id: int, todo: schemas.TodoUpdate, db: Session = Depends
 
 # Удаление todo по id
 @app.delete("/todos/delete_{todo_id}", response_model=schemas.Todo)
-async def add_todo(todo_id: int, todo: schemas.TodoUpdate, db: Session = Depends(get_db)):
-    pass
+async def delete_todo(todo_id: int, todo: schemas.Todo, db: Session = Depends(get_db)):
+    db_todo = crud.delete_todo_by_id(db, todo_id=todo_id, todo=todo)
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="Запись с таким id не найдена")
+    return db_todo
