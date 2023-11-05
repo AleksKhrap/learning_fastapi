2485e080afcac47bb37a0d28c40d1f95c288afa7
AleksKhrap <112804992+AleksKhrap@users.noreply.github.com>
2023-11-05 20:50:05 +0300

    Add custom exception handler

--- todo/app/crud.py
+++ todo/app/crud.py
@@ -1,7 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from models import models, schemas
 from sqlalchemy.future import select
-from fastapi import HTTPException
 
 
 async def add_todo(db: AsyncSession, todo: schemas.TodoCreate):
@@ -20,8 +19,6 @@ async def get_todos(db: AsyncSession, skip: int, limit: int):
 
 async def get_todo_by_id(db: AsyncSession, todo_id: int):
     todo = await db.execute(select(models.Todo).filter(models.Todo.todo_id == todo_id))
-    if todo is None:
-        raise HTTPException(status_code=404, detail="Запись с таким id не найдена")
     # db.query(models.Todo).filter(models.Todo.todo_id == todo_id).first()
     return todo.scalar()
 
@@ -29,7 +26,7 @@ async def get_todo_by_id(db: AsyncSession, todo_id: int):
 async def update_todo_by_id(db: AsyncSession, todo_id: int, todo: schemas.TodoUpdate):
     db_todo = await get_todo_by_id(db, todo_id)
     if db_todo is None:
-        raise HTTPException(status_code=404, detail="Запись с таким id не найдена")
+        return db_todo
     db_todo.title, db_todo.description, db_todo.completed = todo.title, todo.description, todo.completed
     await db.commit()
     await db.refresh(db_todo)
@@ -39,7 +36,7 @@ async def update_todo_by_id(db: AsyncSession, todo_id: int, todo: schemas.TodoUp
 async def delete_todo_by_id(db: AsyncSession, todo_id: int):
     db_todo = await get_todo_by_id(db, todo_id)
     if db_todo is None:
-        raise HTTPException(status_code=404, detail="Запись с таким id не найдена")
+        return db_todo
     await db.delete(db_todo)
     await db.commit()
     return {"message": "Запись успешно удалена"}
--- todo/app/main.py
+++ todo/app/main.py
@@ -1,11 +1,35 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Request
 from sqlalchemy.ext.asyncio import AsyncSession
 from models import models, schemas
-from . import crud
+import crud
 from models.database import AsyncSessionLocal, engine
 from typing import List
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
+
+
+class CustomException(HTTPException):
+    def __init__(self, detail: str, status_code: int = 400):
+        super().__init__(status_code=status_code, detail=detail)
+
+
+# Кастомный обработчик ошибок
+@app.exception_handler(CustomException)
+async def custom_exception_handler(request: Request, exc: CustomException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"error": exc.detail}
+    )
+
+
+# Обработчик глобальных исключений, который "ловит" все необработанные исключения
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=500,
+        content={"error": "Ошибка сервера"}
+    )
 
 
 # Установка соединения с БД и создание таблиц (для синхронного просто models.Base.metadata.create_all(bind=engine))
@@ -25,6 +49,7 @@ async def add_todo(todo_create: schemas.TodoCreate, db: AsyncSession = Depends(g
     return await crud.add_todo(db=db, todo=todo_create)
 
 
+# Получение определенного количества todo
 @app.get("/todos", response_model=List[schemas.Todo])
 async def read_todos(db: AsyncSession = Depends(get_db)):
     return await crud.get_todos(db=db, skip=0, limit=10)
@@ -33,13 +58,18 @@ async def read_todos(db: AsyncSession = Depends(get_db)):
 # Получение todo по id
 @app.get("/todos/{todo_id}", response_model=schemas.Todo)
 async def read_todo(todo_id: int, db: AsyncSession = Depends(get_db)):
-    return await crud.get_todo_by_id(db=db, todo_id=todo_id)
+    todo = await crud.get_todo_by_id(db=db, todo_id=todo_id)
+    if todo is None:
+        raise CustomException(status_code=404, detail="Запись с таким id не найдена")
+    return todo
 
 
 # Обновление todo по id
 @app.put("/todos/update_{todo_id}", response_model=schemas.Todo)
 async def update_todo(todo_id: int, todo: schemas.TodoUpdate, db: AsyncSession = Depends(get_db)):
     db_todo = await crud.update_todo_by_id(db, todo_id=todo_id, todo=todo)
+    if db_todo is None:
+        raise CustomException(status_code=404, detail="Запись с таким id не найдена")
     return db_todo
 
 
@@ -47,4 +77,6 @@ async def update_todo(todo_id: int, todo: schemas.TodoUpdate, db: AsyncSession =
 @app.delete("/todos/delete_{todo_id}")
 async def delete_todo(todo_id: int, db: AsyncSession = Depends(get_db)):
     db_todo = await crud.delete_todo_by_id(db, todo_id=todo_id)
+    if db_todo is None:
+        raise CustomException(status_code=404, detail="Запись с таким id не найдена")
     return db_todo
